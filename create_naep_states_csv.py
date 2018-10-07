697b5fec30dcaef68f641055db01db85b754d684
Justin <jgarrard@cableone.net>
2018-10-07 14:10:19 -0400

    Refined output for NAEP data

--- create_naep_states_csv.py
+++ create_naep_states_csv.py
@@ -6,11 +6,13 @@ data on states into a single CSV file.
 import re
 import zipfile
 import os
+import numpy as np
 import pandas as pd
 import xlrd  # Support for older excel files, used by pd
 import us  # US metadata, like state names
 
 
+OUTPUT_FILENAME_BASE = 'naep_states_base.csv'
 OUTPUT_FILENAME = 'naep_states.csv'
 
 # The name of the zip file being unpacked
@@ -55,10 +57,55 @@ def nde_spreadsheet_to_dataframe(filename):
     # Clean up the year by removing the superscript
     data['YEAR'] = data['YEAR'].apply(lambda x: x[:4])
 
+    # Capitalize state names if necessary
+    data['STATE'] = data['STATE'].apply(lambda x: x.upper())
+
+    # Replace spaces with underscore in state names
+    data['STATE'] = data['STATE'].apply(lambda x: re.sub(' ', '_', x.strip()))
+
     # Drop the unused demographic column
     data = data.drop('DEMO', axis=1)
 
     return data
+
+
+def naep_summarize_dataframe(naep_df):
+    """
+
+    :param naep_df:
+    :return:
+    """
+    # Replace '—' with NaN's
+    naep_df.replace('—', np.nan, inplace=True)
+
+    # Convert entries into an easy-to-join format
+    summary_df = pd.DataFrame()
+
+    # We only want one of these per every test
+    summary_df['STATE'] = naep_df[(naep_df['TEST_SUBJECT'].str.contains('Mathematics')) &
+                                  (naep_df['TEST_YEAR'].str.contains('4'))]['STATE']
+
+    summary_df['YEAR'] = naep_df[(naep_df['TEST_SUBJECT'].str.contains('Mathematics')) &
+                                 (naep_df['TEST_YEAR'].str.contains('4'))]['YEAR']
+
+    # We want an entry for each subject/year combination
+    summary_df['AVG_MATH_4_SCORE'] = naep_df[(naep_df['TEST_SUBJECT'].str.contains('Mathematics')) &
+                                             (naep_df['TEST_YEAR'].str.contains('4'))]['AVG_SCORE']
+
+    summary_df['AVG_MATH_8_SCORE'] = naep_df[(naep_df['TEST_SUBJECT'].str.contains('Mathematics')) &
+                                             (naep_df['TEST_YEAR'].str.contains('8'))]['AVG_SCORE']
+
+    summary_df['AVG_READING_4_SCORE'] = naep_df[(naep_df['TEST_SUBJECT'].str.contains('Reading')) &
+                                                (naep_df['TEST_YEAR'].str.contains('4'))]['AVG_SCORE']
+
+    summary_df['AVG_READING_8_SCORE'] = naep_df[(naep_df['TEST_SUBJECT'].str.contains('Reading')) &
+                                                (naep_df['TEST_YEAR'].str.contains('8'))]['AVG_SCORE']
+
+    pk = summary_df['YEAR'] + '_' + summary_df['STATE']
+
+    summary_df.insert(0, 'PRIMARY_KEY', pk)
+
+    return summary_df
 
 
 def main():
@@ -78,6 +125,9 @@ def main():
     output = pd.concat(record)
 
     # Write to file as CSV
+    output.to_csv(OUTPUT_FILENAME_BASE, index=False)
+
+    output = naep_summarize_dataframe(output)
     output.to_csv(OUTPUT_FILENAME, index=False)
 
     # Clean up
