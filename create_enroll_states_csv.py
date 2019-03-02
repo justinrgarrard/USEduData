60acb58855693b8f05bcf5ed0339e67f89f99e19
jgarrard <jgarrard@cableone.net>
2019-03-02 12:27:58 -0500

    Partial refactor.

--- create_enroll_states_csv.py
+++ create_enroll_states_csv.py
@@ -22,45 +22,6 @@ OUTPUT_FILENAME = 'enroll_states.csv'
 # The name of the zip file being unpacked
 ZIP_NAME = 'nces_enroll.zip'
 
-# A schema that matches the column structure
-# Can be modified to include additional columns
-SCHEMA = ['SURVYEAR',
-          'STATENAME',
-          'STNAME',
-          'STATE',
-          'TOTAL',
-          'MEMBER']
-
-# It would've been annoying to hard code each variation, so let's do it this way
-base = ['PK', 'KG', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', 'UG', 'AE', 'AL']
-gr = ['PK', 'KG', 'G01', 'G02', 'G03', 'G04', 'G05', 'G06', 'G07', 'G08', 'G09', 'G10', 'G11', 'G12', 'G13', 'UG', 'AE', 'AL']
-race_mod = ['AM', 'AS', 'HI', 'BL', 'WH', 'HP', 'TR']
-gender_mod = ['M', 'F']
-
-# Grade, All Races and All Genders
-for item in gr:
-    SCHEMA.append(item)
-
-# Race, All Grades and All Genders
-for item in race_mod:
-    SCHEMA.append(item)
-
-# Race + Grade
-for grade in base:
-    for race in race_mod:
-        column_name = race + grade
-        SCHEMA.append(column_name)
-
-# Race + Grade + Gender
-for grade in base:
-    for race in race_mod:
-        for gender in gender_mod:
-            column_name = race + grade + gender
-            SCHEMA.append(column_name)
-
-SCHEMA = list(set(SCHEMA))
-SCHEMA.sort()
-
 # State names
 STATES = us.STATES
 
@@ -68,10 +29,60 @@ STATES = us.STATES
 doublespace = re.compile(r'  ')
 numbersonly = re.compile(r'\d+')
 
+# Column names used for generating the schema
+base = ['PK', 'KG', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+        '10', '11', '12', '13', 'UG', 'AE', 'AL']
+gr = ['PK', 'KG', 'G01', 'G02', 'G03', 'G04', 'G05', 'G06', 'G07', 'G08',
+         'G09', 'G10', 'G11', 'G12', 'G13', 'UG', 'AE', 'AL']
+race_mod = ['AM', 'AS', 'HI', 'BL', 'WH', 'HP', 'TR']
+gender_mod = ['M', 'F']
 
-def nces_spreadsheet_to_dataframe(filename, logger=None):
+
+def gen_schema():
+    """
+    A function that creates extends the schema with columns
+    appropriate to the enrollment data.
+    :return: SCHEMA
+    :rtype list
+    """
+    schema = ['SURVYEAR',
+              'STATENAME',
+              'STNAME',
+              'STATE',
+              'TOTAL',
+              'MEMBER']
+
+    # Grade, All Races and All Genders
+    for item in gr:
+        schema.append(item)
+
+    # Race, All Grades and All Genders
+    for item in race_mod:
+        schema.append(item)
+
+    # Race + Grade
+    for grade in base:
+        for race in race_mod:
+            column_name = race + grade
+            schema.append(column_name)
+
+    # Race + Grade + Gender
+    for grade in base:
+        for race in race_mod:
+            for gender in gender_mod:
+                column_name = race + grade + gender
+                schema.append(column_name)
+
+    schema = list(set(schema))
+    schema.sort()
+
+    return schema
+
+
+def nces_spreadsheet_to_dataframe(filename, schema, logger=None):
     """
     Converts an NCES data spreadsheet to a Pandas dataframe.
+    Performs several cleaning operations to format the input data.
 
     :param filename: The name of the xls file.
     :return: A dictionary with the U.S. states as keys.
@@ -81,60 +92,64 @@ def nces_spreadsheet_to_dataframe(filename, logger=None):
     # Give the user a heads-up
     logger.debug('Parsing ' + str(filename) + '...')
 
-    # Open the file, parse it, and truncate the highlights
-    # print(SCHEMA)
-    d = pd.read_excel(filename, sheet_name=0, dtype=str)
+    # Parse the input file
+    data = pd.read_excel(filename, sheet_name=0, dtype=str)
 
-    # Fixup for whitespace in column names
-    d.rename(columns=lambda x: x.strip(), inplace=True)
+    def state_name_fixups():
+        """
+        Fixes for standardizing state names in input data.
+        :return:
+        """
+        # Standardize 'state name' column
+        if data['STATE'].isnull().values.any():
+            if data['STATENAME'].isnull().values.any():
+                data['STATE'] = data['STNAME']
+            else:
+                data['STATE'] = data['STATENAME']
+        data.drop(['STNAME', 'STATENAME'], axis=1)
 
-    for column in SCHEMA:
-        if column not in d.columns:
-            d[column] = np.nan
+        # Standardize 'total enrollment' column
+        if True in data['TOTAL'].isnull():
+            data['TOTAL'] = data['MEMBER']
+        data.drop('MEMBER', axis=1)
 
-    data = d[SCHEMA]
+        # Standardize state names (no abbreviations)
+        if 'AZ' in data['STATE'].values:
+            abbr_to_name = us.states.mapping('abbr', 'name')
+            for key in abbr_to_name.keys():
+                abbr_to_name[key] = abbr_to_name[key].upper()
+            data['STATE'].replace(abbr_to_name, inplace=True)
 
-    # Add a year, because the source spreadsheets are unreliable
+        # Standardize state names (consistent capitalization)
+        data['STATE'] = data['STATE'].apply(lambda x: x.upper())
+
+        # Standardize state names (underscores, not spaces)
+        data['STATE'] = data['STATE'].apply(lambda x: re.sub(' ', '_', x.strip()))
+
+    # Fix whitespace in column names
+    data.rename(columns=lambda x: x.strip(), inplace=True)
+    for column in schema:
+        if column not in data.columns:
+            data[column] = np.nan
+    data = data[schema]
+
+    # Set the year using the filename
     data['SURVYEAR'] = re.findall(numbersonly, filename)[0]
 
-    # Synonymous columns fixup
-    if data['STATE'].isnull().values.any():
-        if data['STATENAME'].isnull().values.any():
-            data['STATE'] = data['STNAME']
-        else:
-            data['STATE'] = data['STATENAME']
-    data.drop(['STNAME', 'STATENAME'], axis=1)
-
-    if True in data['TOTAL'].isnull():
-        data['TOTAL'] = data['MEMBER']
-    data.drop('MEMBER', axis=1)
-
-    # Some years only used abbreviations for states
-    # Change those to full state names
-    if 'AZ' in data['STATE'].values:
-        abbr_to_name = us.states.mapping('abbr', 'name')
-        for key in abbr_to_name.keys():
-            abbr_to_name[key] = abbr_to_name[key].upper()
-        data['STATE'].replace(abbr_to_name, inplace=True)
-
-    # Capitalize state names if necessary
-    data['STATE'] = data['STATE'].apply(lambda x: x.upper())
-
-    # Replace spaces with underscore in state names
-    data['STATE'] = data['STATE'].apply(lambda x: re.sub(' ', '_', x.strip()))
+    state_name_fixups()
 
     # Drop empty rows
     data = data.dropna(how='all')
     data = data[data['STATE'] != 'NAN']
 
-    # Get rid of trailing whitespace from all strings
+    # Get rid of trailing whitespace from all columns
     for column in data.columns:
         try:
             data[column] = data[column].map(lambda x: x.strip())
         except:
             pass
 
-    # Cast appropriate columns to numbers, removing non-number symbols
+    # Cast columns for numeric data, removing non-number symbols
     for column in data.columns:
         if 'STATE' not in column:
             data[column] = pd.to_numeric(data[column], errors='coerce')
@@ -145,60 +160,8 @@ def nces_spreadsheet_to_dataframe(filename, logger=None):
 
     # Sort
     data.sort_values(['PRIMARY_KEY'], inplace=True)
+
     return data
-
-
-# def enroll_summarize_dataframe(enroll_df):
-#     """
-#
-#     :return:
-#     """
-#     # Translate the -1, -2, and M values to NaN's
-#     enroll_df.replace(-1, np.nan, inplace=True)
-#     enroll_df.replace(-2, np.nan, inplace=True)
-#
-#     # Create summary dataframe
-#     summary_df = pd.DataFrame()
-#
-#     # Create summary columns for state and year
-#     summary_df['PRIMARY_KEY'] = enroll_df['SURVYEAR'].astype(str) + '_' + enroll_df['STATE']
-#     summary_df['STATE'] = enroll_df['STATE']
-#     summary_df['YEAR'] = enroll_df['SURVYEAR']
-#
-#     # Create summary columns for grades
-#     def create_summary_grades(prefix):
-#         if len(prefix) == 0:
-#             prefix = 'G'
-#             summary_df['GRADES_PK_' + prefix] = enroll_df['PK']
-#             summary_df['GRADES_KG_' + prefix] = enroll_df['KG']
-#         else:
-#             summary_df['GRADES_PK_' + prefix] = enroll_df[prefix + 'PK']
-#             summary_df['GRADES_KG_' + prefix] = enroll_df[prefix + 'KG']
-#
-#         summary_df['GRADES_4_' + prefix] = enroll_df[prefix + '04']
-#         summary_df['GRADES_8_' + prefix] = enroll_df[prefix + '08']
-#         summary_df['GRADES_12_' + prefix] = enroll_df[prefix + '12']
-#
-#         summary_df['GRADES_1_8_' + prefix] = enroll_df[prefix + '01'] + enroll_df[prefix + '02'] + \
-#                                    enroll_df[prefix + '03'] + enroll_df[prefix + '04'] + \
-#                                    enroll_df[prefix + '05'] + enroll_df[prefix + '06'] + \
-#                                    enroll_df[prefix + '07'] + enroll_df[prefix + '08']
-#
-#         summary_df['GRADES_9_12_' + prefix] = enroll_df[prefix + '09'] + enroll_df[prefix + '10'] + \
-#                                     enroll_df[prefix + '11'] + enroll_df[prefix + '12']
-#
-#         summary_df['GRADES_ALL_' + prefix] = summary_df['GRADES_PK_' + prefix] + summary_df['GRADES_1_8_' + prefix] + \
-#                                              summary_df['GRADES_9_12_' + prefix]
-#
-#     create_summary_grades('')
-#
-#     # for race in race_mod:
-#     #     create_summary_grades(race)
-#
-#     # Sort
-#     summary_df.sort_values(['YEAR', 'STATE'])
-#
-#     return summary_df
 
 
 def enroll_summarize_dataframe(enroll_df):
@@ -206,7 +169,7 @@ def enroll_summarize_dataframe(enroll_df):
 
     :return:
     """
-    # Translate the -1, -2, and M values to NaN's
+    # Translate the -1 and -2 values to NaN's
     enroll_df.replace(-1, np.nan, inplace=True)
     enroll_df.replace(-2, np.nan, inplace=True)
 
@@ -218,7 +181,6 @@ def enroll_summarize_dataframe(enroll_df):
     summary_df['STATE'] = enroll_df['STATE']
     summary_df['YEAR'] = enroll_df['SURVYEAR']
 
-    # Create summary columns for grades
     def create_summary_grades(prefix, suffix=''):
         """
         Create summary values by race (prefix) and gender (suffix).
@@ -290,7 +252,6 @@ def enroll_summarize_dataframe(enroll_df):
     for race in race_mod:
         race_summary_fixup(race)
 
-
     # Sort
     summary_df.sort_values(['PRIMARY_KEY'])
 
@@ -305,11 +266,14 @@ def main(logger=None):
     out.extractall(os.getcwd())
     out.close()
 
+    # Generate the schema
+    schema = gen_schema()
+
     # Iterate through spreadsheets, extracting data
     record = []
     for item in file_list:
         if '/.' not in item:
-            df = nces_spreadsheet_to_dataframe(item, logger=logger)
+            df = nces_spreadsheet_to_dataframe(item, schema, logger=logger)
             record.append(df)
 
     # Glue the annual surveys into a single file
