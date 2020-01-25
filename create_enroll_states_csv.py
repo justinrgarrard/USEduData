cbb6a2fa966c747845759e14cd8be4abdf3d1261
Justin Garrard <justinrgarrard@gmail.com>
2020-01-25 10:35:23 -0500

    Getting closer; got a file with the proper schema

--- create_enroll_states_csv.py
+++ create_enroll_states_csv.py
@@ -18,7 +18,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 INPUT_FILENAME = 'enroll_states_raw.csv'
 
 # The name of the output CSVs
-OUTPUT_FILENAME = 'enroll_states_trial.csv'
+OUTPUT_FILENAME = 'enroll_states.csv'
 
 # State names
 STATES = us.STATES
@@ -58,15 +58,14 @@ def restructure_enroll_data(input_df):
 
     # For each state and year, create a primary key
     output_df = pd.DataFrame()
-
     primary_key = []
     for state in STATES:
         for year in year_range:
-            primary_key.append('{0}_{1}'.format(state, year))
+            primary_key.append('{0}_{1}'.format(year, str(state).upper()))
 
     output_df['PRIMARY_KEY'] = primary_key
-    output_df['STATE'] = [x.split('_')[0] for x in output_df['PRIMARY_KEY']]
-    output_df['YEAR'] = [x.split('_')[1] for x in output_df['PRIMARY_KEY']]
+    output_df['STATE'] = [x.split('_')[1] for x in output_df['PRIMARY_KEY']]
+    output_df['YEAR'] = [x.split('_')[0] for x in output_df['PRIMARY_KEY']]
 
     # Convert each <YEAR_GRADE_RACE_GENDER> column,
     # producing <GRADE_RACE_GENDER> columns
@@ -79,7 +78,6 @@ def restructure_enroll_data(input_df):
     # Populate the output dataframe using the input dataframe
     ## There has to be a nicer way to do this...
     for col_name in data_cols:
-        # print(col_name)
         # Parse the column name
         specs = find_specs(col_name)
         year = specs[0]
@@ -89,15 +87,15 @@ def restructure_enroll_data(input_df):
 
         # Import rows from the input data into the output dataframe
         column_data = input_df[['State Name', col_name]]
-        # print(column_data)
         target_col = '{0}_{1}_{2}'.format(grade, race, gender)
         target_col_data = output_df[['STATE', 'YEAR', target_col]]
         target_col_data = target_col_data[(target_col_data['YEAR'].str.contains(year))]
 
         ## Avoid dealing with index issues by converting to list
         target_col_data[target_col] = column_data[col_name].to_list()
-        # print(target_col_data)
-        # print('')
+
+        # Set the column type to numeric, filtering out strings
+        target_col_data[target_col] = pd.to_numeric(target_col_data[target_col], errors='coerce')
 
         output_df[target_col].update(target_col_data[target_col])
 
@@ -110,7 +108,6 @@ def main(logger=None, input_dir=None, output_dir=None):
 
     # Transform the data (YEAR_STATE format)
     output_df = restructure_enroll_data(input_data)
-    print(output_df)
 
     # Output as file
     output_data_path = os.path.join(output_dir, OUTPUT_FILENAME)
