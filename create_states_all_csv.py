aaf4fb907fcb431ce1ea383bc2e5526b0f637072
Justin Garrard <justinrgarrard@gmail.com>
2020-04-11 10:52:26 -0400

    Patched a regression in create_states, properly removing spaces from state names

--- create_states_all_csv.py
+++ create_states_all_csv.py
@@ -14,6 +14,7 @@ FINANCE_FILENAME = 'finance_states.csv'
 ENROLL_FILENAME = 'enroll_states_summary.csv'
 ENROLL_EXTENDED_FILENAME = 'enroll_states.csv'
 ACHIEVE_FILENAME = 'naep_states.csv'
+ACHIEVE_EXTENDED_FILENAME = 'naep_states.csv'
 
 OUTPUT_FILENAME = 'states_all.csv'
 OUTPUT_EXTENDED_FILENAME = 'states_all_extended.csv'
@@ -24,9 +25,6 @@ STATES = us.STATES
 # Useful regular expressions
 doublespace = re.compile(r'  ')
 numbersonly = re.compile(r'\d+')
-
-# Year range
-yr_range = np.arange(1992, 2016, 1)
 
 
 def main(logger=None, input_dir=None, output_dir=None, sanity_dir=None):
@@ -50,7 +48,7 @@ def main(logger=None, input_dir=None, output_dir=None, sanity_dir=None):
     # Combine extended data
     finance_data = pd.read_csv(os.path.join(input_dir, FINANCE_FILENAME))
     enroll_data = pd.read_csv(os.path.join(input_dir, ENROLL_EXTENDED_FILENAME))
-    achieve_data = pd.read_csv(os.path.join(input_dir, ACHIEVE_FILENAME))
+    achieve_data = pd.read_csv(os.path.join(input_dir, ACHIEVE_EXTENDED_FILENAME))
 
     all_data = finance_data.merge(enroll_data, on=['PRIMARY_KEY', 'STATE', 'YEAR'], how='outer')
 
