bcebdc94d87934b0e196dc718445e955aeb12957
Justin Garrard <justinrgarrard@gmail.com>
2020-12-20 11:50:29 -0700

    Expanded achievement pipeline testing, finding a possible regression

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,7 @@ output files have been generated.
 import os
 import time
 import shutil
+import numpy as np
 import pandas as pd
 import unittest
 import zipfile
@@ -175,17 +176,38 @@ class AchievementStatePipelineTests(unittest.TestCase):
         output_filename = 'naep_states_raw.csv'
         output_data = pd.read_csv(os.path.join(OUTPUT_DIR, output_filename))
 
-        # Test one entry to ensure that the transformed value matches
-        ## Uses a hardcoded value from the source spreadsheet for input
-        input_val = 223
+        # Test various entries to ensure that the transformed value matches
+        ## Uses hardcoded values from the source spreadsheet for input
+        input_vals_hardcoded = {'2019': 223,
+                     '2017': 223,
+                     '2015': 222,
+                     '2013': 219,
+                     '2011': 221,
+                     '2009': 221,
+                     '2007': 223,
+                     '2005': 222,
+                     '2003': 218,
+                     '2002': 220,
+                     '2000': np.nan,
+                     '1998': np.nan,
+                     '1994': np.nan,
+                     '1992': 219
+                     }
 
-        output_row = output_data[output_data['YEAR'] == 2019]
-        output_row = output_row[output_row['STATE'] == 'IDAHO']
-        output_row = output_row[output_row['DEMO'] == 'G04_A_A']
-        output_row = output_row[output_row['TEST_SUBJECT'] == 'Reading']
-        output_val = output_row['AVG_SCORE'].iloc[0]
-
-        assert (input_val == output_val)
+        for year, input_val in input_vals_hardcoded.items():
+            try:
+                output_row = output_data[output_data['YEAR'] == int(year)]
+                output_row = output_row[output_row['STATE'] == 'IDAHO']
+                output_row = output_row[output_row['DEMO'] == 'G04_A_A']
+                output_row = output_row[output_row['TEST_SUBJECT'] == 'Reading']
+                output_val = output_row['AVG_SCORE'].iloc[0]
+                if np.isnan(input_val):
+                    assert (np.isnan(output_val))
+                else:
+                    assert (input_val == output_val)
+            except:
+                print(f'{year}:{input_val}')
+                raise Exception
 
     def test_raw_to_standard(self):
         # Load in data
@@ -194,17 +216,27 @@ class AchievementStatePipelineTests(unittest.TestCase):
         output_filename = 'naep_states.csv'
         output_data = pd.read_csv(os.path.join(OUTPUT_DIR, output_filename))
 
-        # Test one entry to ensure that the transformed value matches
-        input_row = input_data[input_data['YEAR'] == 2019]
-        input_row = input_row[input_row['STATE'] == 'IDAHO']
-        input_row = input_row[input_row['DEMO'] == 'G04_A_A']
-        input_row = input_row[input_row['TEST_SUBJECT'] == 'Reading']
-        input_val = input_row['AVG_SCORE'].iloc[0]
+        # Test various entries to ensure that the transformed value matches
+        input_val_years = ['2019', '2017', '2015', '2013', '2011', '2009', '2007',
+                           '2005', '2003', '2002', '2000', '1998', '1994', '1992']
+        for year in input_val_years:
+            try:
+                input_row = input_data[input_data['YEAR'] == int(year)]
+                input_row = input_row[input_row['STATE'] == 'IDAHO']
+                input_row = input_row[input_row['DEMO'] == 'G04_A_A']
+                input_row = input_row[input_row['TEST_SUBJECT'] == 'Reading']
+                input_val = input_row['AVG_SCORE'].iloc[0]
 
-        output_row = output_data[output_data['PRIMARY_KEY'] == '2019_IDAHO']
-        output_val = output_row['G04_A_A_READING'].iloc[0]
+                output_row = output_data[output_data['PRIMARY_KEY'] == '2019_IDAHO']
+                output_val = output_row['G04_A_A_READING'].iloc[0]
 
-        assert(input_val == output_val)
+                if np.isnan(input_val):
+                    assert (np.isnan(output_val))
+                else:
+                    assert (input_val == output_val)
+            except:
+                print(f'{year}')
+                raise Exception
 
     def test_standard_to_summary(self):
         # Load in data
@@ -213,14 +245,25 @@ class AchievementStatePipelineTests(unittest.TestCase):
         output_filename = 'naep_states_summary.csv'
         output_data = pd.read_csv(os.path.join(OUTPUT_DIR, output_filename))
 
-        # Test one entry to ensure that the transformed value matches
-        input_row = input_data[input_data['PRIMARY_KEY'] == '2019_IDAHO']
-        input_val = input_row['G04_A_A_READING'].iloc[0]
+        # Test various entries to ensure that the transformed value matches
+        input_val_years = ['2019', '2017', '2015', '2013', '2011', '2009', '2007',
+                           '2005', '2003', '2002', '2000', '1998', '1994', '1992']
+        for year in input_val_years:
+            try:
+                input_row = input_data[input_data['PRIMARY_KEY'] == f'{year}_IDAHO']
+                input_val = input_row['G04_A_A_READING'].iloc[0]
 
-        output_row = output_data[output_data['PRIMARY_KEY'] == '2019_IDAHO']
-        output_val = output_row['AVG_READING_4_SCORE'].iloc[0]
+                output_row = output_data[output_data['PRIMARY_KEY'] == f'{year}_IDAHO']
+                output_val = output_row['AVG_READING_4_SCORE'].iloc[0]
 
-        assert (input_val == output_val)
+                if np.isnan(input_val):
+                    assert (np.isnan(output_val))
+                else:
+                    assert (input_val == output_val)
+
+            except:
+                print(f'{year}')
+                raise Exception
 
     def test_standard_to_all(self):
         # Load in data
@@ -229,14 +272,25 @@ class AchievementStatePipelineTests(unittest.TestCase):
         output_filename = 'states_all_extended.csv'
         output_data = pd.read_csv(os.path.join(OUTPUT_DIR, output_filename))
 
-        # Test one entry to ensure that the transformed value matches
-        input_row = input_data[input_data['PRIMARY_KEY'] == '2019_IDAHO']
-        input_val = input_row['G04_A_A_READING'].iloc[0]
+        # Test various entries to ensure that the transformed value matches
+        input_val_years = ['2019', '2017', '2015', '2013', '2011', '2009', '2007',
+                           '2005', '2003', '2002', '2000', '1998', '1994', '1992']
+        for year in input_val_years:
+            try:
+                input_row = input_data[input_data['PRIMARY_KEY'] == f'{year}_IDAHO']
+                input_val = input_row['G04_A_A_READING'].iloc[0]
 
-        output_row = output_data[output_data['PRIMARY_KEY'] == '2019_IDAHO']
-        output_val = output_row['G04_A_A_READING'].iloc[0]
+                output_row = output_data[output_data['PRIMARY_KEY'] == f'{year}_IDAHO']
+                output_val = output_row['G04_A_A_READING'].iloc[0]
 
-        assert (input_val == output_val)
+                if np.isnan(input_val):
+                    assert (np.isnan(output_val))
+                else:
+                    assert (input_val == output_val)
+
+            except:
+                print(f'{year}')
+                raise Exception
 
 
 if __name__ == '__main__':
